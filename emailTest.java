28c737eb410c35bdce1b855e420a25091a372cb8
Ashwath Naidu <84516184+Ashwath-0704@users.noreply.github.com>
2021-12-29 18:14:56 +0530

    Update emailTest.java

--- emailTest.java
+++ emailTest.java
@@ -11,41 +11,6 @@ public class emailTest{
     /**
      * Email Test :-)
      */
-	@Test
-    public void firstNameTest(){
-    	boolean firstResult=email.firstName();
-    	Assert.assertEquals(true,firstResult);
-    }
-	@Test
-    public void lastNameTest() {
-		boolean lastresult =email.lastName();
-    	Assert.assertEquals(true,lastresult);
-    }
-	@Test
-    public void phoneNumberTest(){
-		boolean phoneNumberresult = email.phonenumber();
-    	Assert.assertEquals(true,phoneNumberresult);
-    }
-	@Test
-    public void passwordRule1Test(){
-		boolean passwordRule1Result = email.passwordRule1();
-    	Assert.assertEquals(true,passwordRule1Result);
-    }
-	@Test
-    public void passwordRule2Test(){
-		boolean passwordRule2Result =email.passwordRule2();
-    	Assert.assertEquals(true,passwordRule2Result);
-    }
-	@Test
-    public void passwordRule3Test(){
-		boolean passwordRule3Result = email.passwordRule3();
-    	Assert.assertEquals(true,passwordRule3Result);
-    }
-	@Test
-    public void passwordRule4Test(){
-		boolean passwordRule4Result=email.passwordRule4();
-    	Assert.assertEquals(true, passwordRule4Result);
-    }
 	/*|----------------------------------------------------|
 	 *|		(Valid) 			|		(Invalid)		   |	
 	 *|-------------------------|--------------------------|
