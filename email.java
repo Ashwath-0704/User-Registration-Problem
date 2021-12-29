811a9d145f5f0a45e8c88665005f5e68c3fb326b
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-29 13:57:17 +0530

    UC3

--- email.java
+++ email.java
@@ -29,9 +29,26 @@ public class email {
 		Matcher m = p.matcher(lastName);
 		System.out.println(lastName + "--->" + m.matches());
 	}
+
+	/*
+	 * As a User need to enter a valid email - E.g. abc.xyz@bl.co.in - Email has 3
+	 * mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and .
+	 * positions
+	 */
+	public static String emailId() {
+		String emailId = JOptionPane.showInputDialog("Enter email id");
+		String regex = "[A-Za-z0-9.+-]*[a-zA-Z0-9]+[@][a-z0-9]+[.][a-z]+[.]?[a-z]+";
+//		String regex = "^(?=.[A-Za-z0-9.+-])+@([a-z]{1,}.[a-z]{2,})"; // doutu
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(emailId);
+		System.out.println(emailId + "--->" + m.matches());
+		return emailId;
+	}
+
 	public static void main(String[] args) {
 		
 //		firstName(); //UC1
-		lastName();  //UC2
+//		lastName();  //UC2
+		emailId(); //UC3
 	}
 }
