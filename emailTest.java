f1212759645fb4cf6b37ee366f93ff0821fdfd45
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-29 14:07:09 +0530

    UC9Testing

---
+++ emailTest.java
@@ -0,0 +1,27 @@
+package com.javapractices;
+
+import org.junit.Test;
+import Regax.email;
+
+/**
+ * Unit test for simple App.
+ */
+public class emailTest{
+    /**
+     * Email Test :-)
+     */
+	/*|----------------------------------------------------|
+	 *|		(Valid) 			|		(Invalid)		   |	
+	 *|-------------------------|--------------------------|
+	 *| 	abc@yahoo.com 		|		abc123@.com		   |
+	 *|  	abc-100@yahoo.com 	|		abc.@gmail.com	   |	
+	 *| 	abc.100@yahoo.com 	|		abc@gmail.com.1a   |  	
+	 *| 	abc111@abc.com		|    	abc@abc@gmail.com  |
+	 *| 	abc+100@gmail.com  	|		abc@gmail.com.1a   |
+	 *|----------------------------------------------------|
+	 */
+    @Test
+    public void shouldAnswerWithTrue(){
+    	email.emailTesting();
+    }
+}
