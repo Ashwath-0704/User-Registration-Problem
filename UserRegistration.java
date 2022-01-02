1ceacafbe8a7c723152300ba9f94cd72e1bd33bd
Ashwath Naidu <84516184+Ashwath-0704@users.noreply.github.com>
2022-01-02 12:48:17 +0530

    Update and rename email.java to UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.regex.*;
 import javax.swing.JOptionPane;
 
-public class email {
+public class UserRegistration {
 	/*
 	 * As a User need to enter a valid First Name -First name starts with Cap and
 	 * has minimum 3 characters
@@ -15,7 +15,7 @@ public class email {
 		final String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(firstName);
-		
+
 		if (firstName != null) {
 			if (m.matches() == true) {
 				System.out.println(firstName + "--->" + m.matches());
@@ -48,8 +48,6 @@ public class email {
 	 * mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and .
 	 * positions
 	 */
-
-
 	public static boolean emailId(String emailId) throws InvalidUserInputException {
 //		String emailId = JOptionPane.showInputDialog("Enter email id");
 		final String regex = "[A-Za-z0-9.+-]*[a-zA-Z0-9]+[@][a-z0-9]+[.][a-z]+[.]?[a-z]+";
@@ -64,6 +62,7 @@ public class email {
 		}
 		throw new InvalidUserInputException();
 	}
+
 	/*
 	 * As a User need to follow pre -defined Mobile Format - E.g. 91 9919819801 -
 	 * Country code follow by space and 10 digit number
@@ -162,8 +161,6 @@ public class email {
 	public static String emailIdSting() {
 		String emailId = JOptionPane.showInputDialog("Enter email id");
 		final String regex = "[A-Za-z0-9.+-]*[a-zA-Z0-9]+[@][a-z0-9]+[.][a-z]+[.]?[a-z]+";
-//		String regex = "^(?=.[A-Za-z0-9.+-])+@([a-z]{1,}.[a-z]{2,})"; // doutu
-//		String regex = "^[A-Za-z0-9.+-]+@[a-z0-9]+[.][a-z]+$"; 
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(emailId);
 		System.out.println(emailId + "--->" + m.matches());
